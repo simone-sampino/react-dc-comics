8bd3c286e11fcb554ccafdc6b6a17b4053ca13c1
simone-sampino <s.samp98@gmail.com>
2025-07-14 19:54:18 +0200

    Update Main component with new assets

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,30 @@
+import digitalComics from '/img/digital-comics.png'
+import merchandise from '/img/merchandise.png'
+import subs from '/img/subscriptions.png'
+import shopLocator from '/img/shop-locator.png'
+import powerVisa from '/img/power-visa.svg'
+
 export default function Main() {
     return (
-        <main className="bg-black">
-            
+        <main className='text-white'>
+            <div className='bg-black'>
+            <p className="container d-flex align-items-center">--&gt; Content goes here &lt;--</p>
+            </div>
+
+            <div className="bg-primary">
+                <div id='shop' className='container d-flex align-items-center justify-content-evenly'>
+                    <img src={digitalComics} alt="Digital Comics"/>
+                        <span>digital comics</span>
+                    <img src={merchandise} alt="Merchandise"/>
+                        <span>dc merchandise</span>
+                    <img src={subs} alt="Subscriptions"/>
+                        <span>subscription</span>
+                    <img src={shopLocator} alt="Shop Locator"/>
+                        <span>comic shop locator</span>
+                    <img src={powerVisa} alt="Power Visa"/>
+                        <span>dc power visa</span>
+                </div>
+            </div>
         </main>
     )
 }
