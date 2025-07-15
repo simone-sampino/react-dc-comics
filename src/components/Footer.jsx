cb8ae8849790a93e27ecc409d9af169ff4b2ade8
simone-sampino <s.samp98@gmail.com>
2025-07-15 11:43:06 +0200

    Refactor Footer layout and styles; update Main component to render shop items dynamically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,54 +7,61 @@ import periscope from '/img/periscope.png'
 export default function Footer() {
     return (
         <footer>
+            <div className="bg-comics">
             <div className="container text-white">
-                <div className="row d-flex flex-wrap">
-                    <div className="col flex-grow-1">
-                        <h3>dc comics</h3>
-                        <ul>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Videos</a></li>
-                            <li><a href="#">News</a></li>
-                        </ul>
+                <div className="row">
+                    <div className="col-6 links">
+                        <div className="row d-flex flex-wrap">
+                            <div className="col-3">
+                                <h3>dc comics</h3>
+                                <ul>
+                                    <li><a href="#">Characters</a></li>
+                                    <li><a href="#">Comics</a></li>
+                                    <li><a href="#">Movies</a></li>
+                                    <li><a href="#">TV</a></li>
+                                    <li><a href="#">Games</a></li>
+                                    <li><a href="#">Videos</a></li>
+                                    <li><a href="#">News</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-3">
+                                <h3>dc</h3>
+                                <ul>
+                                    <li><a href="#">Terms Of Use</a></li>
+                                    <li><a href="#">Privacy policy (New)</a></li>
+                                    <li><a href="#">Ad Choices</a></li>
+                                    <li><a href="#">Advertising</a></li>
+                                    <li><a href="#">Jobs</a></li>
+                                    <li><a href="#">Subscriptions</a></li>
+                                    <li><a href="#">Talent Workshops</a></li>
+                                    <li><a href="#">CPSC Certificates</a></li>
+                                    <li><a href="#">Ratings</a></li>
+                                    <li><a href="#">Shop Help</a></li>
+                                    <li><a href="#">Contact US</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-3">
+                                <h3>sites</h3>
+                                <ul>
+                                    <li><a href="#">DC</a></li>
+                                    <li><a href="#">MAD Magazine</a></li>
+                                    <li><a href="#">DC Kids</a></li>
+                                    <li><a href="#">DC Universe</a></li>
+                                    <li><a href="#">DC Power Visa</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-12">
+                                <h3>shop</h3>
+                                <ul>
+                                    <li><a href="#">Shop DC</a></li>
+                                    <li><a href="#">Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+                        </div>  
                     </div>
-                    <div className="col">
-                        <h3>dc</h3>
-                        <ul>
-                            <li><a href="#">Terms Of Use</a></li>
-                            <li><a href="#">Privacy policy (New)</a></li>
-                            <li><a href="#">Ad Choices</a></li>
-                            <li><a href="#">Advertising</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Subscriptions</a></li>
-                            <li><a href="#">Talent Workshops</a></li>
-                            <li><a href="#">CPSC Certificates</a></li>
-                            <li><a href="#">Ratings</a></li>
-                            <li><a href="#">Shop Help</a></li>
-                            <li><a href="#">Contact US</a></li>
-                        </ul>
-                    </div>
-                    <div className="col">
-                        <h3>sites</h3>
-                        <ul>
-                            <li><a href="#">DC</a></li>
-                            <li><a href="#">MAD Magazine</a></li>
-                            <li><a href="#">DC Kids</a></li>
-                            <li><a href="#">DC Universe</a></li>
-                            <li><a href="#">DC Power Visa</a></li>
-                        </ul>
-                    </div>
-                    <div className="col">
-                        <h3>shop</h3>
-                        <ul>
-                            <li><a href="#">Shop DC</a></li>
-                            <li><a href="#">Shop DC Collectibles</a></li>
-                        </ul>
-                    </div>
+                    <div className="col-6 bg-dc"></div>
                 </div>
+            </div>
             </div>
                 <div className="footer-bottom">
                     <div className="container d-flex justify-content-between">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,39 @@ import subs from '/img/subscriptions.png'
 import shopLocator from '/img/shop-locator.png'
 import powerVisa from '/img/power-visa.svg'
 
+const shop = [
+    {
+        id: 1,
+        src: digitalComics,
+        alt: "Digital Comics",
+        span: 'digital comics',
+    },
+    {
+        id: 2,
+        src: merchandise,
+        alt: "Merchandise",
+        span: 'dc merchandise',
+    },
+    {
+        id: 3,
+        src: subs,
+        alt: "Subscriptions",
+        span: 'subscription',
+    },
+    {
+        id: 4,
+        src: shopLocator,
+        alt: "Shop Locator",
+        span: 'comic shop locator',
+    },
+    {
+        id: 5,
+        src: powerVisa,
+        alt: "Power Visa",
+        span: 'dc power visa',
+    }
+]
+
 export default function Main() {
     return (
         <main className='text-white'>
@@ -13,6 +46,15 @@ export default function Main() {
 
             <div className="bg-primary">
                 <div id='shop' className='container d-flex align-items-center justify-content-evenly'>
+                    {shop.map(({id, src, alt, span}) => (
+                        <div key={id}>
+                            <img src={src} alt={alt}/>
+                            <span>{span}</span>
+                        </div>
+                    ))}
+                </div>
+
+                {/* <div id='shop' className='container d-flex align-items-center justify-content-evenly'>
                     <img src={digitalComics} alt="Digital Comics"/>
                         <span>digital comics</span>
                     <img src={merchandise} alt="Merchandise"/>
@@ -23,7 +65,7 @@ export default function Main() {
                         <span>comic shop locator</span>
                     <img src={powerVisa} alt="Power Visa"/>
                         <span>dc power visa</span>
-                </div>
+                </div> */}
             </div>
         </main>
     )
